2f1f324d52e23c0454261bf8188006a9e65b1961
Kristina Kraft <kristina.kraft@proway.treina>
2023-09-15 17:20:22 -0300

    pasta exercicio1header

---
+++ js/barra.js
@@ -0,0 +1,7 @@
+document.write("<nav>")
+document.write("<ul>")
+document.write("<li><a href='./pages/Idade.html' target='main'>Idade</a></li>")
+document.write("<li><a href='./pages/Media.html' target='main'>Media</a></li>")
+document.write("<li><a href='./pages/ICM.html' target='main'>ICM</a></li>")
+document.write("<li><a href='./pages/Aumento.html' target='main'>Aumento</a></li>")
+
---
+++ js/header.js
@@ -0,0 +1,10 @@
+document.write("<header>")
+document.write("<div id='titulo'>")        
+document.write("<h1>Frida Maria</h1>")
+document.write("<img class='logo'c='./imagens/Beige_Minimal_Personal_Make_Up_Artist_Logo__2_-removebg-preview.png' width='200px' height='200px'>")
+document.write("</div>")
+document.write("</header>")
+
+
+
+
